ec99c84bd0ca19f387b8604245772468a9b764b8
ymao <ymao@hydro.washington.edu>
2015-05-27 22:04:20 -0700

    Fixed an output flow precision problem in RBM code (Write.90)

--- model_run/source/RBM/Write.f90
+++ model_run/source/RBM/Write.f90
@@ -9,6 +9,6 @@ real   :: T_head
 real   :: dbt
 real   :: Q_out
 !
-write (20,'(f12.4,4i6,3f8.2,f9.0,f8.1,f8.4,f10.4)')           &                                              
+write (20,'(f12.4,4i6,3f8.2,f10.1)')           &                                              
             time,nd,nr,ncell,ns,T_0,T_head,dbt,Q_out
 end SUBROUTINE WRITE
