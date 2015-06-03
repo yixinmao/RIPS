9dc9e0bc98ac7da03f59625a3c8f61e604efef98
ymao <ymao@hydro.washington.edu>
2015-06-02 17:46:12 -0700

    Added scripts for plotting RBM results and compare with USGS data

--- model_run/result_analysis/scripts/cmp_flow.py
+++ model_run/result_analysis/scripts/cmp_flow.py
@@ -1,28 +1,38 @@
 #!/usr/local/anaconda/bin/python
 
+# This script plots routed flow together with USGS observed streamflow
+
 import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
+import sys
 import my_functions
 
 #========================================================
 # Parameter setting
 #========================================================
-rbm_output_formatted_path = '/raid2/ymao/VIC_RBM_east/VIC_RBM/model_run/output/RBM/Maurer_8th/Tennessee/Tennessee_1949_2010/35.0625_-85.6875_reach299_seg2'  # year; month; day; flow(cfs); T_stream(deg); 1 header line
+rbm_output_formatted_path = sys.argv[1] # '/raid2/ymao/VIC_RBM_east/VIC_RBM/model_run/output/RBM/Maurer_8th/Tennessee/Tennessee_1949_2010/35.0625_-85.6875_reach299_seg2'  # year; month; day; flow(cfs); T_stream(deg); 1 header line
 
-usgs_site_code = '03571850'
-usgs_site_name = 'Tennessee River at Lower Pittsburgh'
-usgs_data_path = '../data/USGS_data/streamflow_' + usgs_site_code + '.txt'
-usgs_data_col = 1
+usgs_site_code = sys.argv[2] # '03571850'
+usgs_site_name = sys.argv[3] # 'Tennessee River at Lower Pittsburgh'
+usgs_data_path = '../data/USGS_data/Tennessee/' + usgs_site_code + '.txt'
+usgs_flow_col = sys.argv[4] # format: 1 or 1&2
+# check how many data columns there are
+ave_flag = 0  # 0 for only one data column; 1 for more than one column, need to average
+if len(usgs_flow_col.split('&'))>1:  # if more than one column, need to average these c
+	ave_flag = len(usgs_streamT_col.split('&'))
+else:
+	usgs_flow_col = int(usgs_flow_col)
 
-output_plot_basename = '../output/Tennessee_flow_' + usgs_site_code # output plot path basename (suffix will be added to different plots)
+
+output_plot_basename = '../output/Tennessee_' + usgs_site_code # output plot path basename (suffix will be added to different plots)
 
 #-------------------------------------------------
 
-plot_start_date = dt.datetime(1950, 10, 1)  # start date shown on the plot (should be complete water years)
-plot_end_date = dt.datetime(1987, 9, 30)  # end date shown on the plot
+#plot_start_date = dt.datetime(1950, 10, 1)  # start date shown on the plot (should be complete water years)
+#plot_end_date = dt.datetime(1987, 9, 30)  # end date shown on the plot
 
-time_locator = ('year', 5)  # time locator on the plot; 'year' for year; 'month' for month. e.g., ('month', 3) for plot one tick every 3 months
+#time_locator = ('year', 5)  # time locator on the plot; 'year' for year; 'month' for month. e.g., ('month', 3) for plot one tick every 3 months
 
 #-------------------------------------------------
 
@@ -33,15 +43,42 @@ time_locator = ('year', 5)  # time locator on the plot; 'year' for year; 'month'
 rbm_data = np.loadtxt(rbm_output_formatted_path, skiprows=1)  # year; month; day; flow(cfs); T_stream(deg)
 rbm_date = my_functions.convert_YYYYMMDD_to_datetime(rbm_data[:,0], rbm_data[:,1], rbm_data[:,2])
 df_rbm = my_functions.convert_time_series_to_df(rbm_date, rbm_data[:,3], ['flow'])  # convert to pd.DataFrame
+s_rbm = df_rbm.ix[:,0]  # convert df to Series
 
 # USGS flow
-df_usgs = my_functions.read_USGS_data(usgs_data_path, columns=[1], names=['flow'])  # [cfs]
+if ave_flag==0:  # if only one needed data column
+	df_usgs = my_functions.read_USGS_data(usgs_data_path, columns=[usgs_flow_col], names=['flow'])  # [degC]
+	s_usgs= df_usgs.ix[:,0]  # convert df to Series
+else:  # if more than one data column needed, take average
+	usgs_flow_col_split = usgs_flow_col.split('&')
+	names=[]
+	for i in range(len(usgs_flow_col_split)):
+		usgs_flow_col_split[i] = int(usgs_flow_col_split[i])
+		names.append('flow%d' %i)
+	df_usgs = my_functions.read_USGS_data(usgs_data_path, columns=usgs_flow_col_split, names=names)  # read in data
+	s_usgs = df_usgs.mean(axis=1, skipna=False) # if either column is missing, return NaN
+
+#========================================================
+# Determine plot starting and ending date (always plot full water years)
+#========================================================
+# determine the common range of available data of both data sets
+data_avai_start_date, data_avai_end_date = my_functions.find_data_common_range(s_rbm, s_usgs)
+if (data_avai_start_date-data_avai_end_date).days>=0: # if no common time range
+	print "No common range data available!"
+	exit()
+# find the full water years with available data for both data sets
+plot_start_date, plot_end_date = my_functions.find_full_water_years_within_a_range(data_avai_start_date, data_avai_end_date)
+# determine time locator
+if plot_end_date.year-plot_start_date.year < 5:  # if less than 5 years
+	time_locator = ('year', 1)  # time locator on the plot; 'year' for year; 'month' for month. e.g., ('month', 3) for plot one tick every 3 months
+else:  # if at least 5 years
+	time_locator = ('year', (plot_end_date.year-plot_start_date.year)/5)  # time locator on the plot; 'year' for year; 'month' for month. e.g., ('month', 3) for plot one tick every 3 months
 
 #========================================================
 # Select data to be plotted
 #========================================================
-df_rbm_to_plot = my_functions.select_time_range(df_rbm, plot_start_date, plot_end_date)
-df_usgs_to_plot = my_functions.select_time_range(df_usgs, plot_start_date, plot_end_date)
+s_rbm_to_plot = my_functions.select_time_range(s_rbm, plot_start_date, plot_end_date)
+s_usgs_to_plot = my_functions.select_time_range(s_usgs, plot_start_date, plot_end_date)
 
 #========================================================
 # plot
@@ -49,14 +86,51 @@ df_usgs_to_plot = my_functions.select_time_range(df_usgs, plot_start_date, plot_
 #============== plot daily data ===============#
 fig = plt.figure()
 ax = plt.axes()
-plt.plot_date(df_usgs_to_plot.index, df_usgs_to_plot.flow, 'b-', label='USGS gauge')
-plt.plot_date(df_rbm_to_plot.index, df_rbm_to_plot.flow, 'r--', label='Lohmann route')
+plt.plot_date(s_usgs_to_plot.index, s_usgs_to_plot, 'b-', label='USGS gauge')
+plt.plot_date(s_rbm_to_plot.index, s_rbm_to_plot, 'r--', label='Lohmann route')
 plt.ylabel('Flow (cfs)', fontsize=16)
 plt.title('%s, %s' %(usgs_site_name, usgs_site_code), fontsize=16)
 plt.legend()
 my_functions.plot_date_format(ax, time_range=(plot_start_date, plot_end_date), locator=time_locator, time_format='%Y/%m')
 
-fig = plt.savefig('%s.daily.png' %output_plot_basename, format='png')
+fig = plt.savefig('%s.flow.daily.png' %output_plot_basename, format='png')
 
-plt.show()
+#============== plot monthly data ===============#
+# calculate
+s_usgs_mon = my_functions.calc_monthly_data(s_usgs_to_plot)
+s_rbm_mon = my_functions.calc_monthly_data(s_rbm_to_plot)
+# plot
+fig = plt.figure()
+ax = plt.axes()
+plt.plot_date(s_usgs_mon.index, s_usgs_mon, 'b-', label='USGS gauge')
+plt.plot_date(s_rbm_mon.index, s_rbm_mon, 'r--', label='Lohmann route')
+plt.ylabel('Flow (cfs)', fontsize=16)
+plt.title('Monthly, %s, %s' %(usgs_site_name, usgs_site_code), fontsize=16)
+plt.legend()
+my_functions.plot_date_format(ax, time_range=(plot_start_date, plot_end_date), locator=time_locator, time_format='%Y/%m')
+
+fig = plt.savefig('%s.flow.monthly.png' %output_plot_basename, format='png')
+
+#============== plot seasonal data ===============#
+# calculate
+s_usgs_seas = my_functions.calc_ts_stats_by_group(s_usgs_to_plot, 'month', 'mean') # index is 1-12 (month)
+s_rbm_seas = my_functions.calc_ts_stats_by_group(s_rbm_to_plot, 'month', 'mean') # index is 1-12 (month)
+# plot
+fig = plt.figure()
+ax = plt.axes()
+plt.plot_date(s_usgs_seas.index, s_usgs_seas, 'b-', label='USGS gauge')
+plt.plot_date(s_rbm_seas.index, s_rbm_seas, 'r--', label='Lohmann route')
+plt.ylabel('Flow (cfs)', fontsize=16)
+plt.title('%s, %s\nMonthly mean seasonality, water years %d - %d' %(usgs_site_name, usgs_site_code, plot_start_date.year+1, plot_end_date.year), fontsize=16)
+plt.legend()
+# formatting
+plt.xlim([1, 12])
+tick_labels = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Nov','Oct','Nov','Dec']
+my_functions.plot_format(ax, xtick_location=range(1,13), xtick_labels=tick_labels)
+
+fig = plt.savefig('%s.flow.season.png' %output_plot_basename, format='png')
+
+
+
+
 
---
+++ model_run/result_analysis/scripts/my_functions.py
@@ -0,0 +1,294 @@
+#!/usr/local/anaconda/bin/python
+
+#==============================================================
+#==============================================================
+
+def read_USGS_data(file, columns, names):
+	'''This function reads USGS streamflow from the directly downloaded format (date and data are in the 3rd and 4th columns, respectively; data in cfs)
+
+	Input: 
+		file: directly downloaded streamflow file path [str]
+		columns: a list of data colomn numbers, starting from 1. E.g., if the USGS original data has three variables: max_flow, min_flow, mean_flow, and the desired variable is mean_flow, then columns = [3]
+		names: a list of data column names. E.g., ['mean_flow']; must the same length as columns
+
+	Return:
+		a pd.DataFrame object with time as index and data columns (NaN for missing data points)
+
+	Note: returned data and flow might not be continuous if there is missing data!!!
+
+	'''
+
+	import numpy as np
+	import datetime as dt
+	import pandas as pd
+
+	ndata = len(columns)
+	if ndata != len(names):  # check input validity
+		print "Error: input arguments 'columns' and 'names' must have same length!"
+		exit()
+
+	f = open(file, 'r')
+	date_array = []
+	data = []
+	for i in range(ndata):
+		data.append([])
+	while 1:
+		line = f.readline().rstrip("\n")  # read in one line
+		if line=="":
+			break
+		line_split = line.split('\t')
+		if line_split[0]=='USGS':  # if data line
+			date_string = line_split[2]  # read in date string
+			date = dt.datetime.strptime(date_string, "%Y-%m-%d")  # convert date to dt object
+			date_array.append(date)
+
+			for i in range(ndata):  # for each desired data variable
+				col = columns[i]
+				if line_split[3+(col-1)*2] == '':  # if data is missing
+					value = np.nan
+				elif line_split[3+(col-1)*2] == 'Ice':  # if data is 'Ice'
+					value = np.nan
+				else:  # if data is not missing
+					value = float(line_split[3+(col-1)*2])
+				data[i].append(value)
+
+	data = np.asarray(data).transpose()
+	df = pd.DataFrame(data, index=date_array, columns=names)
+	return df
+
+#==============================================================
+#==============================================================
+
+def convert_YYYYMMDD_to_datetime(year, month, day):
+	''' Convert arrays of year, month, day to datetime objects
+	Input:
+		year: an array of years
+		month: an array of months
+		day: an array of days
+		(The three arrays must be the same length)
+	Return:
+		A list of datetime objects
+'''
+
+	import numpy as np
+	import datetime as dt
+
+	# Check if the input arrays are the same length
+	if len(year)!=len(month) or len(year)!=len(day) or len(month)!=len(day):
+		print "Error: the length of input date arrays not the same length!"
+		exit()
+
+	n = len(year)
+	date = []
+	for i in range(n):
+		date.append(dt.datetime(year=np.int(np.round(year[i])), month=np.int(np.round(month[i])), day=np.int(np.round(day[i]))))
+
+	return date
+
+#==============================================================
+#==============================================================
+
+def convert_time_series_to_df(time, data, columns):
+	'''This function converts datetime objects and data array to pandas dataframe object
+
+	Input:
+		time: a list of datetime objects, e.g. [dt.datetime(2011,1,1), dt.datetime(2011,1,3)]
+		data: a 1-D or 2D array of corresponding data; if 2-D, should have the same number of rows as 'time' length
+		columns: a list of column names, the same length as the number of columns of 'data', e.g. ['A', 'B', 'C']
+
+	Return: a dataframe object
+	'''
+
+	import pandas as pd
+	df = pd.DataFrame(data, index=time, columns=columns)
+	return df
+
+#==============================================================
+#==============================================================
+
+def select_time_range(data, start_datetime, end_datetime):
+	''' This function selects out the part of data within a time range
+
+	Input:
+		data: [dataframe/Series] data with index of datetime
+		start_datetime: [dt.datetime] start time
+		end_datetime: [dt.datetime] end time
+
+	Return:
+		Selected data (same object type as input)
+	'''
+
+	import datetime as dt
+
+	start = data.index.searchsorted(start_datetime)
+	end = data.index.searchsorted(end_datetime)
+
+	data_selected = data.ix[start:end+1]
+
+	return data_selected
+
+#==============================================================
+#==============================================================
+
+def plot_date_format(ax, time_range=None, locator=None, time_format=None):
+	''' This function formatting plots by plt.plot_date
+	Input:
+		ax: plotting axis
+		time range: a tuple of two datetime objects indicating xlim. e.g., (dt.date(1991,1,1), dt.date(1992,12,31))
+
+	'''
+
+	import matplotlib.pyplot as plt
+	import datetime as dt
+	from matplotlib.dates import YearLocator, MonthLocator, DateFormatter
+
+	# Plot time range
+	if time_range!=None:
+		plt.xlim(time_range[0], time_range[1])
+
+	# Set time locator (interval)
+	if locator!=None:
+		if locator[0]=='year':
+			ax.xaxis.set_major_locator(YearLocator(locator[1]))
+		elif locator[0]=='month':
+			ax.xaxis.set_major_locator(MonthLocator(interval=locator[1]))
+
+	# Set time ticks format
+	if time_format!=None:
+		ax.xaxis.set_major_formatter(DateFormatter(time_format))
+
+	return ax
+
+#==============================================================
+#==============================================================
+
+def find_first_and_last_nonNaN_index(s):
+	''' This function finds the first and last index with valid column values
+	Input:
+		df: Series object
+		column: data column name to be investigated [str]
+	'''
+
+	import pandas as pd
+
+	first_index = s[s.notnull()].index[0]
+	last_index = s[s.notnull()].index[-1]
+	return first_index, last_index
+
+#==============================================================
+#==============================================================
+
+def find_data_common_range(s1, s2):
+	''' This function determines the common range of valid data of two datasets (missing data in the middle also count as 'valid')
+
+	Input: pd.Series objects
+
+	Return: first and last datetimes of common range (Note: if one or both data have missing data at the beginning or end, those missing periods will be omitted; however, if there is missing data in the middle, those missing points will still be considered as valid)
+
+	Requred:
+		find_first_and_last_nonNaN_index
+	'''
+
+	s1_first_date, s1_last_date =  find_first_and_last_nonNaN_index(s1)
+	s2_first_date, s2_last_date =  find_first_and_last_nonNaN_index(s2)
+	data_avai_start_date = sorted([s1_first_date, s2_first_date])[-1]
+	data_avai_end_date = sorted([s1_last_date, s2_last_date])[0]
+
+	return data_avai_start_date, data_avai_end_date 
+
+#==============================================================
+#==============================================================
+
+def find_full_water_years_within_a_range(dt1, dt2):
+	''' This function determines the start and end date of full water years within a time range
+
+	Input:
+		dt1: time range starting time [dt.datetime]
+		dt2: time range ending time [dt.datetime]
+
+	Return:
+		start and end date of full water years
+	'''
+
+	import datetime as dt
+
+	if dt1.month <= 9:  # if dt1 is before Oct, start from Oct 1 this year
+		start_date_WY = dt.datetime(dt1.year, 10, 1)
+	elif dt1.month==10 and dt1.day==1:  # if dt1 is on Oct 1, start from this date
+		start_date_WY = dt.datetime(dt1.year, 10, 1)
+	else:  # if dt1 is after Oct 1, start from Oct 1 next year
+		start_date_WY = dt.datetime(dt1.year+1, 10, 1)
+
+	if dt2.month >=10:  # if dt2 is Oct or after, end at Sep 30 this year
+		end_date_WY = dt.datetime(dt2.year, 9, 30)
+	elif dt2.month==9 and dt2.day==30:  # if dt2 is on Sep 30, end at this date
+		end_date_WY = dt.datetime(dt2.year, 9, 30)
+	else:  # if dt2 is before Sep 30, end at Sep 30 last year
+		end_date_WY = dt.datetime(dt2.year-1, 9, 30)
+
+	if (end_date_WY-start_date_WY).days > 0:  # if at least one full water year
+		return start_date_WY, end_date_WY
+	else: # else, return -1 
+		return -1
+
+#==============================================================
+#==============================================================
+
+def calc_monthly_data(data):
+	'''This function calculates monthly mean values
+
+	Input: [DataFrame/Series] with index of time
+	Return: a [DataFrame/Series] object, with monthly mean values (the same units as input data)
+	'''
+
+	import pandas as pd
+	data_mon = data.resample("M", how='mean')
+	return data_mon
+
+#==============================================================
+#==============================================================
+
+def calc_ts_stats_by_group(data, by, stat):
+	'''This function calculates statistics of time series data grouped by year, month, etc
+
+	Input:
+		df: a [pd.DataFrame/Series] object, with index of time
+		by: string of group by, (select from 'year' or 'month')
+		stat: statistics to be calculated, (select from 'mean')
+		(e.g., if want to calculate monthly mean seasonality (12 values), by='month' and stat='mean')
+
+	Return:
+		A [dateframe/Series] object, with group as index (e.g. 1-12 for 'month')
+	'''
+
+	import pandas as pd
+
+	if by=='year':
+		if stat=='mean':
+			data_result = data.groupby(lambda x:x.year).mean()
+	elif by=='month':
+		if stat=='mean':
+			data_result = data.groupby(lambda x:x.month).mean()
+
+	return data_result
+
+#==============================================================
+#==============================================================
+
+def plot_format(ax, xtick_location=None, xtick_labels=None):
+	'''This function formats plots by plt.plot
+
+	Input:
+		xtick_location: e.g. [1, 2, 3]
+		xtick_labels: e.g. ['one', 'two', 'three']
+	'''
+
+	import matplotlib.pyplot as plt
+
+	ax.set_xticks(xtick_location)
+	ax.set_xticklabels(xtick_labels)
+
+	return ax
+
+
+ 
